0f65eab9be2ea18d4e4050ba42d0c4534b005d48
George Dimousis <dimousis.georgegmail.com>
2018-07-20 11:47:10 +0300

    gulp list updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var webpack = require('webpack-stream');
 var gulp = require('gulp');
 var sass = require('gulp-sass'); 
 var browserSync = require('browser-sync').create();
-var webpackConfig = require('./webpack-config'); 
+var webpackConfig = require('./build_settings/webpack-config'); 
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -10,6 +10,31 @@ gulp.task('browserSync', function() {
         baseDir: 'dist'
       },
     })
+});
+
+gulp.task('images', function(){
+  gulp.src('src/assets/img/**/*')
+  .pipe(gulp.dest('dist/assets/img'))
+  .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('fonts', function(){
+  gulp.src('src/assets/fonts/**/*')
+  .pipe(gulp.dest('dist/assets/fonts'))
+  .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+gulp.task('svg', function(){
+  gulp.src('src/assets/svg/**/*')
+  .pipe(gulp.dest('dist/assets/svg'))
+  .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 gulp.task('html', function(){
@@ -38,10 +63,13 @@ gulp.task('sass', function () {
   }));
 });
 
-gulp.task('watch', ['browserSync'],  function () {
+gulp.task('watch', ['html', 'sass', 'svg', 'fonts', 'images', 'scripts', 'browserSync' ],  function () {
   gulp.watch('src/assets/scss/**/*.scss', ['sass']);
   gulp.watch('src/assets/js/**/*.js', ['scripts']);
   gulp.watch('src/**/*.{html,php}', ['html']);
+  gulp.watch('src/assets/fonts/**/*', ['fonts']);
+  gulp.watch('src/assets/svg/**/*', ['svg']);
+  gulp.watch('src/assets/img/**/*', ['images']);
 });
 
-gulp.task('default', [ 'html', 'sass', 'scripts' ]);
+gulp.task('default', [ 'html', 'sass', 'svg', 'fonts', 'scripts', 'images' ]);
